5b10f24a6e61947ac70c8709d54847981fb5be38
root <root@worker1.okd.com>
2022-03-01 16:35:40 +0100

    update file

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,17 @@
 # date : 01/03/2022
 #############################################
 if [ "$1" == "--create" ];then
-echo ""
-echo "notre option est create"
-echo ""
 nb_machine=1
 [ "$2" != "" ] && nb_machine=$2
-docker run -tid --name $USER-alpine alpine:latest
 
+for i in $(seq 1 $nb_machine);do
+docker run -tid --name $USER-alpine-$i alpine:latest
+echo "$USER-alpine-$i deja cree"
+done
 echo "j'ai creer ${nb_machine}"
 elif [ "$1" == "--drop" ];then
-echo ""
-echo "notre option est drop"
-echo ""
 
-docker rm -f $USER-alpine
+docker rm -f $(docker ps -a |grep alpine |awk '{print $1}')
 
 
 elif [ "$1" == "--start" ];then
