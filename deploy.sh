6c02e9db22a0d49ae5599c53928a3580a69ee941
root <root@worker1.okd.com>
2022-03-01 11:30:43 +0100

    deploy ajout

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+#######################################################
+# description : deploiemnt de conteneur docker
+#
+# auteur : Zied
+# date : 01/03/2022
+#############################################
+
+echo "
+
+Options : 
+	- --create: lancer des conteneures
+	- --drop : supprimer les conteneurs creer par le script 
+	- --infos : carateriqtiques des conteneur (ip , nom , user)
+	- --start : demarrage des conte
+	- --ansible :  deploy 
+
+
+"
